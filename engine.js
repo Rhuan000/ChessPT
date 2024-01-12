2819a14de1c5ba982be451b49cc23ee93e5adafc
Rhuan Elias <rhuanelias29@gmail.com>
2024-01-11 21:04:29 -0300

    evaluating moves

--- engine.js
+++ engine.js
@@ -4,20 +4,28 @@ export class Engine {
   constructor() {
     this.stockfish = new Worker('./node_modules/stockfish/src/stockfish-nnue-16.js');
     this.turn = "";
+    this.previousMoveNumber = 0;
+    this.moveNumber = 0;
+    this.previousScore = null
+    this.after = false
+    this.score = null
+    this.movesEvalueted = []
+    this.movesScores = []
     this.barChild = document.querySelector('#bar-child');
     this.moveScore = document.querySelector('#move-score');
     this.barChildInitialHeight = this.barChild.offsetHeight;
     this.stockfish.postMessage('uci')
     this.stockfish.postMessage('isready')
     this.stockfish.onmessage = this.onStockfishMessage.bind(this);
+    
   }
 
   onStockfishMessage(event) {
     const data = event.data;
     const splicedData = data.split(' ');
     let i = 0;
-
     for (let content of splicedData) {
+      //Checking if is a line of mate or not yet.
       if (content === "score") {
         let scoreType = splicedData[i + 1];
 
@@ -27,7 +35,7 @@ export class Engine {
           if (this.turn === "b") {
             mateIn *= -1;
           }
-
+          //Increase or decrease 100% of the bar side size.
           if (this.turn === "w") {
             this.barChild.style.height = `${mateIn == 0 || mateIn < 0 ? '100%' : '0px'}`;
           } else {
@@ -36,24 +44,34 @@ export class Engine {
           this.moveScore.innerHTML = `#${mateIn == 0 ? "" : mateIn}`;
 
         } else {
-          let score = splicedData[i + 2];
-          score = parseInt(score, 10);
+          this.score = splicedData[i + 2];
+          this.score = parseInt(this.score, 10);
 
           if (this.turn === "b") {
-            score *= -1;
-            console.log(score);
+            this.score *= -1;
           }
-
-          score = (score / 100).toFixed(2);
-
-          if (score < 0) {
-            this.barChild.style.height = `${this.barChildInitialHeight + (((-1 * score) * 8) * (2 * this.barChildInitialHeight) / 100)}px`;
-            this.moveScore.innerHTML = '-' + Math.abs(score);
-          } else if (score > 0) {
-            this.barChild.style.height = `${this.barChildInitialHeight - ((score * 8) * (2 * this.barChildInitialHeight) / 100)}px`;
-            this.moveScore.innerHTML = '+' + score;
+          
+          this.score = (this.score / 100).toFixed(2);
+          if(this.previousMoveNumber == this.moveNumber){
+              this.movesEvalueted[this.moveNumber] = this.score - this.previousScore
+              
+          } else if(this.previousMoveNumber < this.moveNumber) {
+            this.previousScore = this.movesScores[this.moveNumber -1] ?  this.movesScores[this.moveNumber -1] : 0
           } else {
-            this.moveScore.innerHTML = score;
+            this.previousScore = 0
+          }
+          this.previousMoveNumber = this.moveNumber
+          this.movesScores[this.moveNumber] = this.score
+          
+
+          if (this.score < 0) {
+            this.barChild.style.height = `${this.barChildInitialHeight + (((-1 * this.score) * 8) * (2 * this.barChildInitialHeight) / 100)}px`;
+            this.moveScore.innerHTML = '-' + Math.abs(this.score);
+          } else if (this.score > 0) {
+            this.barChild.style.height = `${this.barChildInitialHeight - ((this.score * 8) * (2 * this.barChildInitialHeight) / 100)}px`;
+            this.moveScore.innerHTML = '+' + this.score;
+          } else {
+            this.moveScore.innerHTML = this.score;
           }
 
           break;
@@ -64,11 +82,16 @@ export class Engine {
     }
   }
 
-  analyzePosition(fen, turn) {
+  analyzePosition(fen, turn, moveNumber) {
     this.turn = turn
+    this.moveNumber = moveNumber
     this.stockfish.postMessage('stop');
     const formatedMessage = `position fen ${fen}`;
     this.stockfish.postMessage(formatedMessage);
+    console.log(fen)
     this.stockfish.postMessage(`go depth ${'25'}`);
+  }
+  getActualScore(){
+    return this.score
   }
 }
--- index.js
+++ index.js
@@ -5,9 +5,10 @@ import { Engine } from "./engine.js";
 var savedPGNHistory;
 var currentNumber;
 var chess = new Chess();
-var lastCheck
+var lastCheck 
+var previousScore = null 
 const engine = new Engine();
-
+let checkMoveInterval
 
 document.getElementById('pgnForm').addEventListener('submit', function (event) {
   event.preventDefault();
@@ -27,7 +28,11 @@ var board1 = Chessboard('board1', {
 function initialPosition() {
   currentNumber = -1;
   board1.position('start');
-  engine.analyzePosition(board1.fen(), "w");
+  engine.analyzePosition(board1.fen(), "w", previousScore, currentNumber);
+  const squares = document.querySelectorAll('.square-55d63');
+  squares.forEach(square => square.classList.remove('highlight-white', 'highlight-black', 'highlight-red'));
+
+
 }
 
 function analyzePGN() {
@@ -41,21 +46,50 @@ function analyzePGN() {
 
   if (chess.fen()) {
     board1.position(chess.fen());
-    engine.analyzePosition(chess.fen(), chess.turn());
+    engine.analyzePosition(chess.fen(), chess.turn(), currentNumber);
     highlightLastMove()
   } else {
     console.error('Invalid PGN. Please enter a valid chess game in PGN format.');
   }
 }
 
+var moveType
 function goToNextMove() {
   if (currentNumber < savedPGNHistory.length - 1) {
+    clearInterval(checkMoveInterval)
     currentNumber = currentNumber + 1;
     chess.load(savedPGNHistory[currentNumber].after);
     board1.position(chess.fen());
-    engine.analyzePosition(chess.fen(), chess.turn());
+    engine.analyzePosition(chess.fen(), chess.turn(), currentNumber);
     highlightLastMove()
+    console.log(engine.getActualScore(), previousScore)
     console.log(isKingInCheck())
+    checkMoveInterval = setInterval(()=>{
+      let evaluatedMove = Math.abs(engine.movesEvalueted[currentNumber])
+      console.log(evaluatedMove)
+      switch (true){
+          case evaluatedMove >= 2:
+            moveType = "blunder"
+            addEvaluateSVG(moveType)
+            break;
+          case evaluatedMove < 2 && evaluatedMove >= 1:
+            moveType = "mistake"
+            addEvaluateSVG(moveType)
+            break;
+          case evaluatedMove >= 0.5 && evaluatedMove < 1:
+            moveType = "inaccuracy"
+            addEvaluateSVG(moveType)
+            break;
+          case(evaluatedMove == "bestMove"):
+            moveType = "bestMove"
+            console.log("Best Move")
+            break
+          default:
+            moveType =  "normal"
+            addEvaluateSVG("empty")
+            break
+      }
+    }, 2000);
 
   } else {
     console.log('already in the last move');
@@ -66,10 +100,9 @@ function goToPreviousMove() {
   if (currentNumber > 0) {
     currentNumber = currentNumber - 1;
     chess.load(savedPGNHistory[currentNumber].after);
-    board1.position(chess.fen());    
-    engine.analyzePosition(chess.fen(), chess.turn());
+    board1.position(chess.fen());
+    engine.analyzePosition(chess.fen(), chess.turn(), currentNumber);
     highlightLastMove()
-    console.log(isKingInCheck())
   } else {
     console.log('Already at the last move.');
   }
@@ -89,7 +122,6 @@ function highlightLastMove() {
       if (square.getAttribute('data-square') === fromMove || square.getAttribute('data-square') === toMove) {
         square.classList.add(`highlight-${colorToHighlight}`);
       } else if(square.getAttribute('data-square') === isKingInCheck()){
-        console.log(square)
         //square.classList.add(`highlight-red`)
         setTimeout(()=>{
           lastCheck = square.children[1]? square.children[1]: square.children[0]
@@ -103,6 +135,7 @@ function highlightLastMove() {
 }
 
 function isKingInCheck() {
+  ///If king is in check will return the King actual square.
   const turn = chess.turn();
 
   //Taking off the null items of array.  
@@ -117,5 +150,32 @@ function isKingInCheck() {
   console.log(kingSquare, chess.inCheck())
   if(chess.inCheck() && chess.get(kingSquare).color === turn){
     return kingSquare
+  }
+}
+
+function addEvaluateSVG(moveType){
+  
+  const toMove = savedPGNHistory[currentNumber]?.to;
+  const svgImage = document.createElement('img');
+
+  svgImage.src = `./svgs/${moveType}.svg`; 
+  svgImage.width = 15; // Set the width as needed
+  svgImage.height = 15; // Set the height as needed]
+  svgImage.style.position = 'absolute';
+  svgImage.style.top = '0%';
+  svgImage.style.left = '75%';
+  svgImage.style.zIndex = '3';
+  svgImage.classList.add('evaluation-svg')
+
+  const existingSvgs = document.querySelectorAll('.evaluation-svg');
+  existingSvgs.forEach(svg => svg.remove());
+  console.log(existingSvgs)
+  const squares = document.querySelectorAll('.square-55d63');
+  for (const square of squares) {
+    if (square.getAttribute('data-square') === toMove) {
+      // Do something with the matching square
+      square.appendChild(svgImage); 
+      break; // Exit the loop
+    }
   }
 }
