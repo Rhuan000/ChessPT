2842aa7fb88ffc56be05748180f766f9fa7e59d8
Rhuan Elias <rhuanelias29@gmail.com>
2024-01-14 00:31:51 -0300

    organizating some code

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 // index.js
 import { Chess } from "./node_modules/chess.js/dist/esm/chess.js";
 import { Engine } from "./engine.js";
+import { evaluateMove } from "./evaluating-process.js";
+import { highlightLastMove } from "./chessboard-highlight.js";
 
-var savedPGNHistory;
-var currentNumber;
-var chess = new Chess();
-var lastCheck 
+export var currentNumber;
+export var savedPGNHistory;
+
+export var chess = new Chess();
+export const engine = new Engine();
+
 var previousScore = null 
-const engine = new Engine();
-let checkMoveInterval
+
 
 document.getElementById('pgnForm').addEventListener('submit', function (event) {
   event.preventDefault();
@@ -56,51 +59,14 @@ function analyzePGN() {
 var moveType
 function goToNextMove() {
   if (currentNumber < savedPGNHistory.length - 1) {
-    clearInterval(checkMoveInterval)
     currentNumber = currentNumber + 1;
     chess.load(savedPGNHistory[currentNumber].after);
     board1.position(chess.fen());
+
     engine.analyzePosition(chess.fen(), chess.turn(), currentNumber);
+    
     highlightLastMove()
-    console.log(engine.getActualScore(), previousScore)
-    console.log(isKingInCheck())
-    checkMoveInterval = setInterval(()=>{
-      let evaluatedMove = Math.abs(engine.movesEvalueted[currentNumber])
-      const toMove = savedPGNHistory[currentNumber]?.to;
-      const fromMove = savedPGNHistory[currentNumber]?.from;
-      const fromToMove = fromMove + toMove
-      console.log(fromToMove, engine.bestNextMove[currentNumber - 1])
-      
-      switch (true){
-        case(engine.bestNextMove[currentNumber -1] == fromToMove):
-          moveType = "bestMove"
-          addEvaluateSVG(moveType)
-          break
-        case evaluatedMove >= 2:
-            moveType = "blunder"
-            addEvaluateSVG(moveType)
-            break;
-          case evaluatedMove < 2 && evaluatedMove >= 1:
-            moveType = "mistake"
-            addEvaluateSVG(moveType)
-            break;
-          case evaluatedMove >= 0.5 && evaluatedMove < 1:
-            moveType = "inaccuracy"
-            addEvaluateSVG(moveType)
-            break;
-          
-            
-            case evaluatedMove <= 0.135:
-              moveType = "excelent"
-              addEvaluateSVG(moveType)
-              break
-            
-          default:
-            moveType =  "good"
-            addEvaluateSVG(moveType)
-            break
-      }
-    }, 500);
+    evaluateMove()
 
   } else {
     console.log('already in the last move');
@@ -116,76 +82,5 @@ function goToPreviousMove() {
     highlightLastMove()
   } else {
     console.log('Already at the last move.');
-  }
-}
-
-function highlightLastMove() {
-  const fromMove = savedPGNHistory[currentNumber]?.from;
-  const toMove = savedPGNHistory[currentNumber]?.to;
-  if (fromMove) {
-    const squares = document.querySelectorAll('.square-55d63');
-    lastCheck?.classList.remove('highlight-red')
-    squares.forEach(square => square.classList.remove('highlight-white', 'highlight-black', 'highlight-red'));
-
-    const colorToHighlight = chess.turn() === 'b' ? 'white' : 'black';
-
-    squares.forEach(square => {
-      if (square.getAttribute('data-square') === fromMove || square.getAttribute('data-square') === toMove) {
-        square.classList.add(`highlight-${colorToHighlight}`);
-      } else if(square.getAttribute('data-square') === isKingInCheck()){
-        //square.classList.add(`highlight-red`)
-        setTimeout(()=>{
-          lastCheck = square.children[1]? square.children[1]: square.children[0]
-          lastCheck.classList.add('highlight-red')
-        }
-        , 400)
-        
-      }
-    });
-  }
-}
-
-function isKingInCheck() {
-  ///If king is in check will return the King actual square.
-  const turn = chess.turn();
-
-  //Taking off the null items of array.  
-  const piecesArray = chess.board().map(row =>
-    row.filter(square => square !== null)
-  );
-  //combining all arrays into a single one with some item. 
-  const flattenedArray = [].concat(...piecesArray);
-
-  let kingSquare = flattenedArray.find(piece => piece.type === 'k' && piece.color === turn);
-  kingSquare = kingSquare.square
-  console.log(kingSquare, chess.inCheck())
-  if(chess.inCheck() && chess.get(kingSquare).color === turn){
-    return kingSquare
-  }
-}
-
-function addEvaluateSVG(moveType){
-  
-  const toMove = savedPGNHistory[currentNumber]?.to;
-  const svgImage = document.createElement('img');
-
-  svgImage.src = `./svgs/${moveType}.svg`; 
-  svgImage.width = 15; // Set the width as needed
-  svgImage.height = 15; // Set the height as needed]
-  svgImage.style.position = 'absolute';
-  svgImage.style.top = '0%';
-  svgImage.style.left = '75%';
-  svgImage.style.zIndex = '3';
-  svgImage.classList.add('evaluation-svg')
-
-  const existingSvgs = document.querySelectorAll('.evaluation-svg');
-  existingSvgs.forEach(svg => svg.remove());
-  const squares = document.querySelectorAll('.square-55d63');
-  for (const square of squares) {
-    if (square.getAttribute('data-square') === toMove) {
-      // Do something with the matching square
-      square.appendChild(svgImage); 
-      break; // Exit the loop
-    }
   }
 }
